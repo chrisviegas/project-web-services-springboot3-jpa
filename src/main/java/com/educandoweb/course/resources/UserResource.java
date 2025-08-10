94c53f6989e1d9732412f219f13bbbfd05ca141a
Christian Viegas <chris.gabriel346@gmail.com>
2025-08-10 03:08:18 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,16 +1,14 @@
 package com.educandoweb.course.resources;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -30,4 +28,11 @@ public class UserResource {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,14 +1,13 @@
 package com.educandoweb.course.services;
 
 
-import java.util.List;
-import java.util.Optional;
-
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.educandoweb.course.entities.User;
-import com.educandoweb.course.repositories.UserRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -24,4 +23,8 @@ public class UserService {
 		Optional<User> obj = repository.findById(Id);
 		return obj.get();
 	}
+
+    public User insert(User user) {
+        return repository.save(user);
+    }
 }
